94aea12c6cff90134dd6c52c4227e0fe5a09cc5e
mcekov <mtsekov@zohomail.eu>
2023-12-30 22:22:10 +0200

    Add Animate in Home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+import { useSnapshot } from "valtio";
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../../config/motion";
+
+import state from "../store";
 
 const Home = () => {
-  return <div>Home</div>;
+  const snap = useSnapshot(state);
+
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header>
+            <img
+              src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default Home;
