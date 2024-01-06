01edeafdf653b5f57fd3ded68d01248e89a4ad85
mcekov <mtsekov@zohomail.eu>
2024-01-06 19:43:30 +0200

    Add CustomButton component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,24 +3,13 @@ import "./App.css";
 import Canvas from "./canvas";
 import Customizer from "./pages/Customizer";
 import Home from "./pages/Home";
-
-import { motion, AnimatePresence } from "framer-motion";
 import { useSnapshot } from "valtio";
-import {
-  headContainerAnimation,
-  headContentAnimation,
-  headTextAnimation,
-  slideAnimation,
-} from "../config/motion";
 
 import state from "./store";
 
 function App() {
-  const snap = useSnapshot(state);
-
   return (
     <main className="app transition-all ease-in">
-      <h1 className="head-text">ThreeJS</h1>
       <Home />
       <Canvas />
       <Customizer />
