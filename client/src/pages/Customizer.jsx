075b42e83f7109bbb4e9e2e4f5eaf42ff8f7ce60
mcekov <mtsekov@zohomail.eu>
2024-01-20 11:24:55 +0200

    Add new components

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
+import state from "../store";
 import config from "../config/config";
+import { download } from "../assets";
+import { downloadCanvasToImage, reader } from "../config/helpers";
+import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
+import { fadeAnimation, slideAnimation } from "../config/motion";
+
 const Customizer = () => {
   return <div>Customizer</div>;
 };
